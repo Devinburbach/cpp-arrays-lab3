c2fb1e85d659b4c89dda0b5c9b17feba7941f78f
Burbach <500152227@fvtc.edu>
2017-02-28 18:29:14 -0600

    did stuffs

--- Lab Ex 3 - Arrays/main.cpp
+++ Lab Ex 3 - Arrays/main.cpp
@@ -5,10 +5,10 @@
 	[ ] Write a program that performs the following:
 		[ ] Prompt the user to input five numbers.
 		[ ] Store the input into either an array or a vector.
-		[x] Once the user is finished entering numbers:
-			[x] Display the five numbers in reverse order.
-			[x] Display the highest number entered.
-			[x] Display the lowest number entered.
+		[ ] Once the user is finished entering numbers:
+			[ ] Display the five numbers in reverse order.
+			[ ] Display the highest number entered.
+			[ ] Display the lowest number entered.
 		[ ] Extra Credit: Allow the user to enter as many numbers as they want(at least 5).
 	[ ] Compress(zip) your program and submit it to Blackboard.
 */
@@ -25,9 +25,14 @@ int main()
 
 	for (int i = 0; i < SIZE; i++)
 	{
-
+		cout << "enter number " << i + 1 << ": ";
+		cin >> arr[i];
 	}
 
+	for (int i = 4; i == 0; i--)
+	{
+		cout << arr[i] << endl;
+	}
 
 	_getch();
 	return 0;
